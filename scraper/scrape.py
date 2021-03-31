e778b0b84ff47d2cddab81b044e1c06cc0605936
mangz <mangza777@gmail.com>
2021-03-31 03:22:34 -0400

    add functionalities to write to google datastore

--- scraper/scrape.py
+++ scraper/scrape.py
@@ -1,30 +1,44 @@
 from google.cloud import datastore
 import requests
 import os
+import time
 
 header = {'axesso-api-key': '6486680f-b86c-4409-864e-83865516896d'}
 
 
 # axesso api for http://api-prd.axesso.de/amz/amazon-search-by-keyword-asin
 def keyword_lookup(key_word):
-    payload = {'keyword': key_word, 'domainCode': 'com', 'sortBy': 'relevanceblender', 'page': 1}
+    max_page = 7
+    page = 1
 
-    axesso_response = \
-        requests.get("http://api-prd.axesso.de/amz/amazon-search-by-keyword-asin",
-                     params=payload,
-                     headers=header,
-                     )
-    # parse response from axesso keyword lookup
-    json_response = axesso_response.json()
-    found_products = json_response['foundProducts']
+    payload = {'keyword': key_word, 'domainCode': 'com', 'sortBy': 'relevanceblender'}
+    asins = []
 
-    return found_products;
+    while page <= max_page:
+        payload['page'] = page
+        axesso_response = \
+            requests.get("http://api-prd.axesso.de/amz/amazon-search-by-keyword-asin",
+                         params=payload,
+                         headers=header,
+                         )
+        # parse response from axesso keyword lookup
+        json_response = axesso_response.json()
+        found_products = json_response['foundProducts']
+
+        # add the found products into one list
+        for i in range(len(found_products)):
+            asins.append(found_products[i])
+
+        # increment to get next page
+        page += 1
+
+    return asins;
 
 
 # axesso api for http://api-prd.axesso.de/amz/amazon-lookup-product to look up product details
-def lookup_product(product_asin):
-    url = "https://www.amazon.com/dp/" + str(product_asin)
-    payload = {'url': url}
+def lookup_product(asin):
+    amazon_url = "https://www.amazon.com/dp/" + str(product_asin)
+    payload = {'url': amazon_url}
     axesso_response = requests.get("http://api-prd.axesso.de/amz/amazon-lookup-product",
                                    params=payload,
                                    headers=header,
@@ -34,22 +48,75 @@ def lookup_product(product_asin):
 
 
 if __name__ == '__main__':
-    #os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "464c9eaf0361c12a3db0fff53c6c0399d8a6f270"
-    #datastore_client = datastore.Client()
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r"C:\Users\mangz\Downloads\justcompare_service_account_key.json"
 
+    print("Looking up keyword... \n")
     found_product_asins = keyword_lookup("coffee machine")
     count = 0
     products_size = len(found_product_asins)
+
     for product_asin in found_product_asins:
         count += 1
 
-        #print("Treating " + count + "/" + str(products_size) + "...\n")
         product = lookup_product(product_asin)
-        #key = datastore_client.key('yo')
-        #entity = datastore.Entity(key=key)
 
+        datastore_client = datastore.Client()
+        key = datastore_client.key("AmazonProduct")
+        entity = datastore.Entity(key=key, exclude_from_indexes=(
+            "acKeywordLink",
+            "addon",
+            "amazonUrl",
+            "answeredQuestions",
+            # "asin",
+            # "categories",
+            # "countReview",
+            "currency",
+            "dealPrice",
+            "features",
+            "fulfilledBy",
+            "imageUrlList",
+            "manufacturer",
+            "minimalQuantity",
+            "pantry",
+            # "price",
+            # "priceSaving",
+            "priceShippingInformation",
+            # "prime",
+            "productDescription",
+            "productDetails",
+            # "productRating",
+            "productTitle",
+            "retailPrice",
+            "salePrice"
+            "shippingPrice"
+            "sizeSelection",
+            "soldBy",
+            "warehouseAvailability"
+        ))
+
+        '''
+        # Handle reviews separately
+        print("Inserting reviews indo database... \n")
+        for review in product['reviews']:
+            review_key = datastore_client.key("Review")
+            review_entity = datastore.Entity(key=review_key, exclude_from_indexes=("text", "date", "rating", "title",
+                                                                                   "userName", "url"))
+            review_entity.update({
+                'text': review['text'],
+                'date': review['date'],
+                'rating': review['rating'],
+                'title': review['title'],
+                'userName': review['userName'],
+                'url': review['url'],
+                'asin': product_asin
+            })
+            datastore_client.put(review_entity)
+        '''
+
+        # Then handle the full product items
         url = "https://www.amazon.com/dp/" + str(product_asin)
-        product_dict = {
+
+        entity.update({
             'acKeywordLink': product['acKeywordLink'],
             'addon': product['addon'],
             'amazonUrl': url,
@@ -74,17 +141,14 @@ if __name__ == '__main__':
             'productRating': product['productRating'],
             'productTitle': product['productTitle'],
             'retailPrice': product['retailPrice'],
-            'reviews': product['reviews'],
             'salePrice': product['salePrice'],
             'shippingPrice': product['shippingPrice'],
             'sizeSelection': product['sizeSelection'],
             'soldBy': product['soldBy'],
             'warehouseAvailability': product['warehouseAvailability']
-        }
-
-        print(product_dict)
-        break
-        #entity.update()
-        #datastore_client.put(entity)
+        })
+        datastore_client.put(entity)
+        print("Inserted " + str(count) + "/" + str(products_size) + "...\n")
+        time.sleep(30)  # add 30 second delay.
 
     print("Inserted all data. Yay!")
